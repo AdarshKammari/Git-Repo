c4cc3493cbe405a878a267a7d920d48a768ce1d1
NAGARRO\adarshkammari <adarsh.kammari@nagarro.com>
2022-12-21 23:56:55 +0530

    new code

--- src/com/maths/BasicOperations.java
+++ src/com/maths/BasicOperations.java
@@ -1,9 +1,29 @@
 package com.maths;
 
-public class BasicOperations {
 
+public class BasicOperations {
+	int x,y,a,b;
+	private String name ="Encapsulation" ;
+	public String Name="Inheritance";
+	public void type() {
+		if (x == y) {
+			System.out.println("There is no information about this class's inhertance"+" "+ "This class is " +name);
+		}
+		else {
+			System.out.println("The Class got Inherited"+" "+Name);
+		}
+	}
+	public void newmethod() {
+		
+		if(a!=b) {
+			System.out.println("Message here");
+			
+		}
+	}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		BasicOperations obj = new BasicOperations();
+		obj.type();
 
 	}
 
