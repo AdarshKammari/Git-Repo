b802e0d86f2a0611a2295bcc25b160546fd60298
NAGARRO\adarshkammari <adarsh.kammari@nagarro.com>
2022-12-22 09:02:59 +0530

    file edited

---
+++ src/com/maths/BasicOperations.java
@@ -0,0 +1,33 @@
+package com.maths;
+
+
+public class BasicOperations {
+	int x,y,a,b;
+	private String name ="Encapsulation" ;
+	public String Name="Inheritance";
+	public void type() {
+		if (x == y) {
+			System.out.println("There is no information about this class's inhertance"+" "+ "This class is " +name);
+		}
+		else {
+			System.out.println("The Class got Inherited"+" "+Name);
+		}
+	}
+	public void newmethod() {
+		
+		if(a!=b) {
+			System.out.println("Message here");
+			
+		}
+	}
+	public void newMethod() {
+		///...this is new method...///
+		// new method //
+	}
+	public static void main(String[] args) {
+		BasicOperations obj = new BasicOperations();
+		obj.type();
+
+	}
+
+}
